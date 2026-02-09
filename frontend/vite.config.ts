d6739f89fc2e32113532ef463fe8d0e1c7b3c6d7
Milind Shekhawat <milindshekhawat10@gmail.com>
2026-02-09 22:27:55 +0530

    update: added rollup visualiser

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [preact(), tailwindcss()],
+    plugins: [preact(), tailwindcss(), visualizer({ gzipSize: true })],
 });
